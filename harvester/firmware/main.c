eb32e38d3807e8ccbcdd1a3d73065c0308549bad
Emmanuel DeVries <edevries@uwaterloo.ca>
2011-11-05 11:29:14 -0400

    Starting afresh! Have basic serial com working.

--- harvester/firmware/main.c
+++ harvester/firmware/main.c
@@ -1,23 +1,23 @@
 /* main.c
 
-2010-11-20
-    got LCD working! the culprits: 1. PORTB defaults to analog output
-    2. 'char' defaults to signed in SDCC!
-    lesson learned: explicitly specify types for portability!
-2010-11-21
-    moved LCD into it's own library with cleaned interface
 2010-11-22
     added i2c code; resolved issue with code pages?
 2010-11-29
     fixed i2c code to avoid read-modify-write issue!
-
+2011-11-04
+    start afresh!
+    led blinking
+    serial port sending 'y' repeatedly
+    serial port sending "hello"
+2011-11-5
+    serial port sending 16-bit integers
+    
 */
 
 #define __16f88
 #include "pic/pic16f88.h"
 
 #include "bitop.h"
-#include "lcd.h"
 #include "i2c.h"
 
 typedef unsigned int config;
@@ -39,11 +39,11 @@ config at 0x2007 __CONFIG = _CP_OFF &
 
 
 //will not be part of lcd library:
-unsigned char Update_Lcd_Buf;
 unsigned char Lcd_Ready;
 
-void lcd_type_char(unsigned char, unsigned char, unsigned char);
-void lcd_write_int(unsigned int, unsigned char, unsigned char, unsigned char);
+unsigned char TX_Buf[16];
+
+void write_int(unsigned int, unsigned char, unsigned char);
 
 
 void intHand(void) __interrupt 0
@@ -55,7 +55,6 @@ void intHand(void) __interrupt 0
         if (led_count > 49) {
             LED_PIN = !LED_PIN;
             led_count = 0;
-            Update_Lcd_Buf = 1;
         } else {
             led_count++;
         }
@@ -70,15 +69,13 @@ void setup(void) {
     //Ports
     TRISA=0b00000000;
     PORTA=0b00000000;
-    TRISB=0b00000000;
+    TRISB=0b00100100;
     PORTB=0b00000000;
 
     CMCON=0b00000111; //Turn off comparator on RA port
     //shouldn't be needed due to POR defaults
     
     ANSEL=0;//This is needed! PORTA defaults to analog input!
-
-    lcd_init(); //should happen before interrupts and timer settings, i think
 
 
     GIE = 1; //Enable interrupts
@@ -87,13 +84,25 @@ void setup(void) {
     OPTION_REG = 4; // 1:32 prescaler, giving XLCD 4.1ms for Cmd cycles
     TMR0IE = 1;
     TMR0 = 0;
+    
+    //serial port (TRISB{5,2} are set above)
+    // 8 bits, ASYNC, 2400 baud, TX only
+    SPBRG = 51;
+    RCSTA = 0b10000000;
+    TXSTA = 0b00100000;
+    
 }
 
 
 void main(void) {
-    unsigned char i;//, temp;
+    unsigned char i, j;//, temp;
     unsigned int sen_long, sen_medm, sen_shrt;
     setup();
+
+    //clear TX Buff
+    for (j = 0; j < 16; j++) {
+        TX_Buf[j] = ' ';
+    }
 
     //turn on
     i2c_start();
@@ -117,11 +126,16 @@ void main(void) {
 
             
     while (1) {
+        if (TXIF) // ready for new word
+        {
+            j++;
+            if (j > 15)
+                j = 0;
+            TXREG = TX_Buf[j];
+        }
 
         if (Lcd_Ready) {
-            //clear LCD
-            for (i=0; i < 32; i++)
-                lcd_type_char(' ', 0, 0xff);
+            Lcd_Ready = 0;
             
             i2c_start();
             i = i2c_tx(0b01110010);
@@ -136,38 +150,29 @@ void main(void) {
             sen_shrt = i2c_rx(1);
             sen_shrt += 256 * i2c_rx(0);
             i2c_stop();
-
             
+/*
             lcd_write_int(i, 1, 0, 1);
             lcd_write_int(sen_long, 0, 0, 5);
             lcd_write_int(sen_medm, 0, 6, 5);
-            lcd_write_int(sen_shrt, 1, 2, 5);
-            
-            /*lcd_type_char('H', 1, 3 + LED_PIN);
-            lcd_type_char('e', 0, 0xff);
-            lcd_type_char('l', 0, 0xff);
-            lcd_type_char('l', 0, 0xff);
-            lcd_type_char('o', 0, 0xff);
-            lcd_type_char(' ', 0, 0xff);
-            lcd_type_char('W', 0, 0xff);
-            lcd_type_char('o', 0, 0xff);
-            lcd_type_char('r', 0, 0xff);
-            lcd_type_char('l', 0, 0xff);
-            lcd_type_char('d', 0, 0xff);
-            lcd_type_char('!', 0, 0xff);*/
-            
-            lcd_update(0, 0);//update from buffer
-            Lcd_Ready = 0; // Unset LCD ready flag for delay
+            lcd_write_int(sen_shrt, 1, 2, 5);*/
+            TX_Buf[0] = 'h';
+            TX_Buf[1] = 'e';
+            TX_Buf[2] = 'l';
+            TX_Buf[3] = 'l';
+            TX_Buf[4] = 'o';
+            TX_Buf[14] = 13;
+            TX_Buf[15] = 10;
+
+            write_int(0xffff, 6, 5);
 
         }
     }
 }
 
 
-
-
-void lcd_write_int(unsigned int num, unsigned char row, unsigned char col, unsigned char num_digits)
-// writes the ascii representation of a number to the LCD
+void write_int(unsigned int num, unsigned char col, unsigned char num_digits)
+// writes the ascii representation of an integer to the TX Buffer
 {
     unsigned int digit, s;
 
@@ -177,56 +182,35 @@ void lcd_write_int(unsigned int num, unsigned char row, unsigned char col, unsig
         case 2: goto two_digits;
         case 1: goto one_digit;
     }
-
+    
 //five_digits:
     digit = num / 10000;
     s = digit * 10000;
     num = num - s;
-    lcd_type_char('0' + digit, row, col);
-    col = 0xff; // next digit's position is auto handled by lcd_typ_char()
+    TX_Buf[col] = '0' + digit;
+    col++;
     
 four_digits:
     digit = num / 1000;
     s = digit * 1000;
     num = num - s;
-    lcd_type_char('0' + digit, row, col);
-    col = 0xff;
+    TX_Buf[col] = '0' + digit;
+    col++;
 
 three_digits:
     digit = num / 100;
     s = 100 * digit;
     num = num - s;
-    lcd_type_char('0' + digit, row, col);
-    col = 0xff;
+    TX_Buf[col] = '0' + digit;
+    col++;
 
 two_digits:
     digit = num / 10;
     s = digit * 10;
     num = num - s;
-    lcd_type_char('0' + digit, row, col);
-    col = 0xff;
+    TX_Buf[col] = '0' + digit;
+    col++;
 
 one_digit:
-    lcd_type_char('0' + num, row, col);
+    TX_Buf[col] = '0' + num;
 }
-
-void lcd_type_char(unsigned char char_buf, unsigned char row, unsigned char col)
-{
-    static unsigned char last_row;
-    static unsigned char last_col;
-    
-    if (col > 15) {     // don't use absolute pos
-        row = last_row;
-        col = last_col + 1;
-        if (col > 15) { // column overflow
-            col = 0;
-            row = !row;
-        }
-    }
-    
-    Lcd_Buf[row][col] = char_buf;
-    
-    last_row = row;
-    last_col = col;
-}
-
