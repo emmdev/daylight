cc417f77502b1394707a3255fc0e3dc5119e2bb0
Emmanuel DeVries <edevries@uwaterloo.ca>
2011-11-12 17:05:10 -0500

    Starting lamp controller afresh w/ PIC

--- lamp controller/firmware/main.c
+++ lamp controller/firmware/main.c
@@ -1,84 +1,174 @@
-#include <avr/io.h>
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
+/* main.c
+
+2010-11-22
+    added i2c code; resolved issue with code pages?
+2010-11-29
+    fixed i2c code to avoid read-modify-write issue!
+2011-11-04
+    start afresh!
+    led blinking
+    serial port sending 'y' repeatedly
+    serial port sending "hello"
+2011-11-5
+    serial port sending 16-bit integers
+*/
+
+#define __16f88
+#include "pic/pic16f88.h"
 
 #include "bitop.h"
 
-#include "stdlib.h"
-#include "stdio.h"
-#include "uart.h"
 
-#define COLOURPORT PORTA
-#define CHMAX 3
-#define RED 0
-#define GREEN 1
-#define BLUE 2
+typedef unsigned int config;
+config at 0x2007 __CONFIG = _CP_OFF &
+ _WDT_OFF &
+ _BODEN_OFF &
+ _PWRTE_OFF &
+ _INTRC_IO &
+ _MCLR_ON &
+ _LVP_OFF;
 
-//! global buffers
-unsigned char internalpwm[CHMAX];
-volatile unsigned char pwm[CHMAX];
 
-FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW); // This line is used to set up file access
+#define LED_PIN RB4
+
+#define nop() \
+_asm\
+    nop\
+_endasm
+
+
+//will not be part of lcd library:
+unsigned char Lcd_Ready;
+
+unsigned char TX_Buf[16];
+
+void write_int(unsigned int, unsigned char, unsigned char);
+
+
+void intHand(void) __interrupt 0
+{
+    static unsigned char led_count;
+
+    if (TMR0IE && TMR0IF) {
+        Lcd_Ready = 1;
+        if (led_count > 49) {
+            LED_PIN = !LED_PIN;
+            led_count = 0;
+        } else {
+            led_count++;
+        }
+
+        TMR0IF=0;
+    }
+}
+
 void setup(void) {
-	//Ports
-	DDRA=0b00000111;
-	PORTA=0b00000000;
-	
-	DDRB=0b00000000;
-	PORTB=0b00000000;
-	
-	//i2c
-	DDRC =0b00000011;
-	PORTC=0b00000011;
-	
-	//UART
-	uart_init(19200);
-	stdout = stdin = &uart_str;
-	printf("Booted!\n");
+    OSCCON=0b01110000; //8MHz
+
+    //Ports
+    TRISA=0b00000000;
+    PORTA=0b00000000;
+    TRISB=0b00100100;
+    PORTB=0b00000000;
+
+    CMCON=0b00000111; //Turn off comparator on RA port
+    //shouldn't be needed due to POR defaults
+    
+    ANSEL=0;//This is needed! PORTA defaults to analog input!
+
+
+    GIE = 1; //Enable interrupts
+
+    //Initialize Timer0 - used for LCD refresh rate and long-term timebase
+    OPTION_REG = 4; // 1:32 prescaler, giving XLCD 4.1ms for Cmd cycles
+    TMR0IE = 1;
+    TMR0 = 0;
+    
+    //serial port (TRISB{5,2} are set above)
+    // 8 bits, ASYNC, 2400 baud, TX only
+    SPBRG = 51;
+    RCSTA = 0b10000000;
+    TXSTA = 0b00100000;
+    
 }
 
-ISR(TIMER0_OVF_vect) {
-	static unsigned char softcount=0xFF;
-	if(++softcount==0) {
-		internalpwm[0]=pwm[0];
-		internalpwm[1]=pwm[1];
-		internalpwm[2]=pwm[2];
-		
-		set_bit(COLOURPORT,0);
-		set_bit(COLOURPORT,1);
-		set_bit(COLOURPORT,2);
-	}
-	#define CHECKANDSTOP(bit) if(softcount==internalpwm[bit]) clear_bit(COLOURPORT,bit);
-	CHECKANDSTOP(0);
-	CHECKANDSTOP(1);
-	CHECKANDSTOP(2);
+
+void main(void) {
+    unsigned char i, j;//, temp;
+    setup();
+
+    //clear TX Buff
+    for (j = 0; j < 16; j++) {
+        TX_Buf[j] = ' ';
+    }
+            
+    while (1) {
+        if (TXIF) // ready for new word
+        {
+            j++;
+            if (j > 15)
+                j = 0;
+            TXREG = TX_Buf[j];
+        }
+
+        if (Lcd_Ready) {
+            Lcd_Ready = 0;
+            
+            TX_Buf[0] = 'h';
+            TX_Buf[1] = 'e';
+            TX_Buf[2] = 'l';
+            TX_Buf[3] = 'l';
+            TX_Buf[4] = 'o';
+            TX_Buf[14] = 13;
+            TX_Buf[15] = 10;
+
+            write_int(0xffff, 6, 5);
+
+        }
+    }
 }
 
-int main(void) {
-	setup();
-	
-	pwm[0]=255;
-	pwm[1]=128;
-	pwm[2]=10;
-	
-	//Prescaler = FCPU
-	TCCR0|=(0<<CS02)|(0<<CS01)|(1<<CS00);
-	
-	//Enable Overflow Interrupt Enable
-	TIMSK|=(1<<TOIE0);
-	
-	//Initialize Counter
-	TCNT0=0;
-	
-	//Enable Global Interrupts
-	sei();
-	
-	while(1) {
-		int r,g,b;
-		scanf("%2x%2x%2x",&r,&g,&b);
-		pwm[0]=r;
-		pwm[1]=g;
-		pwm[2]=b;
-	}
+
+void write_int(unsigned int num, unsigned char col, unsigned char num_digits)
+// writes the ascii representation of an integer to the TX Buffer
+{
+    unsigned int digit, s;
+
+    switch (num_digits) {
+        case 4: goto four_digits;
+        case 3: goto three_digits;
+        case 2: goto two_digits;
+        case 1: goto one_digit;
+    }
+    
+//five_digits:
+    digit = num / 10000;
+    s = digit * 10000;
+    num = num - s;
+    TX_Buf[col] = '0' + digit;
+    col++;
+    
+four_digits:
+    digit = num / 1000;
+    s = digit * 1000;
+    num = num - s;
+    TX_Buf[col] = '0' + digit;
+    col++;
+
+three_digits:
+    digit = num / 100;
+    s = 100 * digit;
+    num = num - s;
+    TX_Buf[col] = '0' + digit;
+    col++;
+
+two_digits:
+    digit = num / 10;
+    s = digit * 10;
+    num = num - s;
+    TX_Buf[col] = '0' + digit;
+    col++;
+
+one_digit:
+    TX_Buf[col] = '0' + num;
 }
